2dd45413fef11a0127bb4778bd3f1e1639e5a98e
Lainezz <diegolinaresmojacar@gmail.com>
2024-11-27 10:35:09 +0100

    adicion de javadoc

--- src/main/java/com/es/jwtsecurity/controller/RutaProtegidaController.java
+++ src/main/java/com/es/jwtsecurity/controller/RutaProtegidaController.java
@@ -1,9 +1,16 @@
 package com.es.jwtsecurity.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ruta_protegida")
 public class RutaProtegidaController {
+
+
+    @GetMapping("/")
+    public String rutaProtegida() {
+        return "Esto es una ruta protegida";
+    }
 }
--- src/main/java/com/es/jwtsecurity/security/SecurityConfig.java
+++ src/main/java/com/es/jwtsecurity/security/SecurityConfig.java
@@ -1,2 +1,75 @@
-package com.es.jwtsecurity.security;public class SecurityConfig {
+package com.es.jwtsecurity.security;
+
+import com.es.jwtsecurity.service.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    /**
+     * BEAN QUE ESTABLECE EL SECURITY FILTER CHAIN
+     * Método que establece una serie de filtros de seguridad para nuestra API
+     * ¿Para qué sirve este método?
+     * Este método sirve para establecer los filtros de seguridad que las peticiones deberán cumplir antes de
+     * llegar al endpoint al que se dirijan
+     *
+     * request ------> filtro1 -> filtro2 -> filtro3 ... -> endpoint
+     *
+     * Por así decirlo, al cargar la aplicación, Spring Security coge lo que tengamos aquí definido y lo "pone"
+     * delante de nuestra app a modo de filtros de seguridad. Esto lo hace automáticamente.
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable()) // Deshabilitamos "Cross-Site Request Forgery" (CSRF) (No lo trataremos en este ciclo)
+                .authorizeHttpRequests(auth -> auth // Filtros para securizar diferentes endpoints de la aplicación
+                        .requestMatchers("/usuarios/login", "/usuarios/register").permitAll() // Filtro que deja pasar todas las peticiones que vayan a los endpoints que definamos
+                        .anyRequest().authenticated() // Para el resto de peticiones, el usuario debe estar autenticado
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+    }
+
+
+    /**
+     * Método que carga en el Spring ApplicationContext un Bean de tipo PasswordEncoder
+     * ¿Por qué este método? ¿Para qué sirve este método?
+     * Este método está aquí para que podamos inyectar donde nos plazca objetos de tipo PasswordEncoder
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /**
+     * Método que carga en el Spring ApplicationContext un Bean de tipo AuthenticationManager
+     * ¿Por qué este método? ¿Para qué sirve este método?
+     * Este método está aquí para que podamos inyectar donde nos plazca objetos de tipo AuthenticationManager
+     * @param authenticationConfiguration
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
 }
